0367c7fc8e473e467cb43523beb23ca6ac173847
Devideas <63525658+AlvinSCPGit@users.noreply.github.com>
2020-12-05 13:17:58 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+import parts
 
+entity_list = []
+
+# Reserved Index for entities
+USER_INDEX = 2
+RES_P1PLANE = 0
+RES_P2PLANE = 1
+
+running = True
+while running:
+	
+	for e in entity_list:
+		e.onTickUpdate(entity_list)
